fc77b20db536f9ac84e0d8268e70873bf0334b2c
Lisa <liza_vater@yahoo.com>
2024-10-05 15:37:20 +0300

    feat: register router

--- backend/app-python/app/schemas/token.py
+++ backend/app-python/app/schemas/token.py
@@ -1,6 +1,11 @@
 from app.common import BaseSchema
+from app.schemas.user import User
 
 
 class TokenCreate(BaseSchema):
     username: str
     password: str
+
+class Token(BaseSchema):
+    access_token: str
+    user: User
--- backend/app-python/app/schemas/user.py
+++ backend/app-python/app/schemas/user.py
@@ -4,3 +4,13 @@ from app.common import BaseSchema
 class User(BaseSchema):
     name: str
     username: str
+
+class UserCreateRequest(BaseSchema):
+    name: str
+    username: str
+    password: str
+
+
+class UserLoginRequest(BaseSchema):
+    username: str
+    password: str
