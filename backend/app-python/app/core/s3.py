93c02ae428f354fc092118ed1977894f04edff88
Ivan Ilin <buran05@icloud.com>
2024-10-05 17:37:49 +0000

    backend: s3 creds

--- backend/app-python/app/core/s3.py
+++ backend/app-python/app/core/s3.py
@@ -1,3 +1,18 @@
 import boto3
+import os
 
-s3_session = boto3.session.Session()
+kwargs = {}
+
+if os.environ.get('AWS_ACCESS_KEY_ID'):
+    kwargs['aws_access_key_id'] = os.environ.get('AWS_ACCESS_KEY_ID')
+
+if os.environ.get('REGION_NAME'):
+    kwargs['region_name'] = os.environ.get('REGION_NAME')
+
+if os.environ.get('AWS_SECRET_ACCESS_KEY'):
+    kwargs['aws_secret_access_key'] = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+
+s3_session = boto3.session.Session(
+    **kwargs
+)
